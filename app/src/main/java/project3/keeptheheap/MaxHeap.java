dfc723a73a1305115b8c19aef51d993e0f56dc02
JFlem <jordane.fleming.7@gmail.com>
2025-04-07 18:13:53 -0700

    Update MaxHeap.java

--- app/src/main/java/project3/keeptheheap/MaxHeap.java
+++ app/src/main/java/project3/keeptheheap/MaxHeap.java
@@ -1,8 +1,15 @@
 package project3.keeptheheap;
-import java.util.Arrays;
 
-public final class MaxHeap<T extends Comparable<? super T>> 
-            implements MaxHeapInterface<T> {
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;;
+
+public final class MaxHeap<T extends Comparable<? super T>>
+        implements MaxHeapInterface<T> {
     private T[] heap;
     private int lastIndex;
     private boolean initialized = false;
@@ -12,6 +19,7 @@ public final class MaxHeap<T extends Comparable<? super T>>
     public MaxHeap() {
         this(DEFAULT_CAPACITY);
     }
+
     public MaxHeap(int initialCapacity) {
         if (initialCapacity < DEFAULT_CAPACITY) {
             initialCapacity = DEFAULT_CAPACITY;
@@ -24,7 +32,7 @@ public final class MaxHeap<T extends Comparable<? super T>>
         }
 
         @SuppressWarnings("unchecked")
-        T[]tempHeap = (T[]) new Comparable[initialCapacity + 1];
+        T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
         heap = tempHeap;
         lastIndex = 0;
         initialized = true;
@@ -42,20 +50,20 @@ public final class MaxHeap<T extends Comparable<? super T>>
 
     @Override
     public void addSequential(T newEntry) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'addSequential'");
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addSequential'");
     }
 
     @Override
     public void addOptimal(T newEntry) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'addOptimal'");
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addOptimal'");
     }
 
     @Override
     public T removeMax() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'removeMax'");
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeMax'");
     }
 
     @Override
@@ -87,5 +95,63 @@ public final class MaxHeap<T extends Comparable<? super T>>
         }
         lastIndex = 0;
     }
-    
+
+    // Using Optimal Method (reheap and heapsort)
+    // Makes an array from the data.txt file once implemented
+    static Integer[] fileToArray(String fileName) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(fileName));
+        int index = 0;
+        Integer[] fileArray = new Integer[100];
+        while (sc.hasNext()) {
+            fileArray[index] = sc.nextInt();
+            index++;
+        }
+        sc.close();
+        return fileArray;
+    }
+
+    void reheap(T[] heap, int rootIndex, int lastIndex) {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex + 1;
+        while (!done && leftChildIndex <= lastIndex) {
+            int largerChildIndex = leftChildIndex;
+            int rightChildIndex = leftChildIndex + 1;
+            if (rightChildIndex <= lastIndex && heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0) {
+                largerChildIndex = rightChildIndex;
+            }
+            if (orphan.compareTo(heap[largerChildIndex]) < 0) {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex + 1;
+            } else {
+                done = true;
+            }
+        }
+        heap[rootIndex] = orphan;
+    }
+
+    // Swaps an array at index a and b
+    void swap(T[] array, int a, int b) {
+        T tempArray = array[a];
+        array[a] = array[b];
+        array[b] = tempArray;
+    }
+
+    void heapsort(T[] array, int n) {
+        for (int rootIndex = n / 2 - 1; rootIndex >= 0; rootIndex--) {
+            reheap(array, rootIndex, n - 1);
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        MaxHeap<Integer> sorter = new MaxHeap<>();
+        Integer[] array = fileToArray("app/src/main/resources/data_sorted.txt");
+        sorter.heapsort(array, array.length);
+
+        System.out.println("Sorted:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
 }
